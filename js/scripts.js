e05e19b935b6912ff0ac326ead5dba0e82ca3e1a
jwnardine <student@epicodus.com>
2015-10-28 19:29:34 -0700

    fixed so it knows when it's not a triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,62 @@
 var triangle = function(sideA,sideB,sideC) {
-  if ((sideA + sideB) < sideC) {
-  alert("This is not a triangle");
-  return "Unknown";
-  }
 
-  if ((sideA + sideC) < sideA) {
-  alert("This is not a triangle");
-  return "Unknown";
-  }
-
-  if ((sideB + sideC) < sideA) {
-  alert("This is not a triangle");
-  return "Unknown";
-  }
-
-  if (sideA === null || sideB === null || sideC === null) {
-    return "Unknown";
-  }
+  // if (sideA === null || sideB === null || sideC === null) {
+  //   return "Unknown";
+  // }
 
   if (sideA === sideB && sideB === sideC) {
-  alert("Equalateral");
+  alert("Your triangle is Equalateral");
   return "Equilateral";
   }
 
+  if ((sideA + sideB) <= sideC) {
+  alert("This is not a triangle");
+  return "Unknown";
+  }
+
   if (sideA === sideB && sideA !== sideC) {
-  alert("Isosceles");
+  alert("Your triangle is Isosceles");
   return "Isosceles";
   }
 
+  if ((sideA + sideC) <= sideB) {
+  alert("This is not a triangle");
+  return "Unknown";
+  }
+
   if (sideA !== sideB && sideA === sideC) {
-  alert("Isosceles");
+  alert("Your triangle is Isosceles");
+  return "Isosceles"
+  }
+
+  if ((sideB + sideC) <= sideA) {
+  alert("This is not a triangle");
+  return "Unknown";
   }
 
   if (sideA !== sideB && sideA !== sideC && sideB === sideC) {
-  alert("Isosceles");
+  alert("Your triangle is Isosceles");
+  return "Isosceles"
   }
 
   if (sideA !== sideB && sideA !== sideC && sideB !== sideC) {
-  alert("Scalene!");
+  alert("Your triangle is Scalene!");
+  return "Scalene"
   }
+
+
+
+
+
+
+
 };
 
 $(document).ready(function(){
   $("#click_me").submit(function(event) {
-    var sideA = prompt("How long is the first side of your triangle?");
-    var sideB = prompt("How long is the second side of your triangle?");
-    var sideC = prompt("How long is the third side of your triangle?");
+    var sideA = parseInt(prompt("How long is the first side of your triangle?"));
+    var sideB = parseInt(prompt("How long is the second side of your triangle?"));
+    var sideC = parseInt(prompt("How long is the third side of your triangle?"));
 
     triangle(sideA,sideB,sideC);
     event.preventDefault();
